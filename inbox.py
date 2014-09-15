87aee336cf48e27647bc73230f85e667730d676e
Roel <nyubis@gmail.com>
2014-09-15 21:50:29 +0300

    Stores mentions now, can retrieve the last few

--- inbox.py
+++ inbox.py
@@ -3,19 +3,43 @@ import re
 
 class inbox(znc.Module):
 	triggers = []
+	storageName = "inboxLines"
 
 	def OnLoad(self, args, msg):
 		self.triggers.append(self.GetUser().GetNick())
-		self.regex = re.compile(makeRegex(), flags=re.IGNORECASE)
+		self.regex = re.compile(self.makeRegex(), flags=re.IGNORECASE)
 		self.PutModule("Loaded triggers: %s" % ",".join(self.triggers))
+		self.lines = self.read()
 		
 		return znc.CONTINUE
 
 	def OnChanMsg(self, nick, channel, msg):
 		if self.regex.search(str(msg.s)) != None:
-			self.PutModule("<%s> %s" % (nick, msg.s))
+			self.lines.append("<%s> %s" % (nick, msg.s))
+			self.write(self.lines)
 
 		return znc.CONTINUE
+
+	def OnModCommand(self, command):
+		if command[:5] == "show ":
+			count = int(command[5:])
+			self.printout(self.lines[-count:])
+		elif command == "debug":
+			self.PutModule("%d lines stored" % len(self.lines))
+			self.PutModule(repr(self.lines))
+
+		return znc.CONTINUE
+
+	def write(self, lines):
+		self.SetNV(self.storageName, "\n".join(lines))
+
+	def read(self):
+		stored = self.GetNV(self.storageName)
+		return stored.split("\n") if stored != '' else []
+
+	def printout(self, lines):
+		for line in lines:
+			self.PutModule(line)
 
 	def makeRegex(self):
 		escaped = map(lambda t: re.escape(t), self.triggers)
